5c6fead0a042289b89be8fe629ae7f634f1081d8
Dmitriy Yartsev <iartsev.dmitrii@student.ehu.lt>
2026-05-31 14:13:14 +0300

    feat: HW7

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { MainLayout } from "@components/layout/MainLayout/MainLayout.jsx";
 import { HomePage } from "./pages/homePage/HomePage.jsx";
 import { MenuPage } from "./pages/menuPage/MenuPage.jsx";
-import { CartProvider } from "./context/CartContext.jsx";
 import { ErrorBoundary } from "@components/UI/errorBoundary/ErrorBoundary.jsx";
-import { BrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
-import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { NotFoundPage } from "./pages/notFoundPage/NotFoundPage.jsx";
 import { MenuDetailPage } from "./pages/menuDetailPage/MenuDetailPage.jsx";
-import { router } from "./providers/router.jsx";
 import { LoginPage } from "./pages/loginPage/LoginPage.jsx";
 import { OrderPage } from "./pages/orderPage/OrderPage.jsx";
 import { RequireAuth } from "@components/features/requireAuth/RequireAuth.jsx";
@@ -14,29 +12,25 @@ import { RequireAuth } from "@components/features/requireAuth/RequireAuth.jsx";
 export default function App() {
   return (
     <ErrorBoundary>
-      <CartProvider>
-        {/*<RouterProvider router={router}>*/}
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<MainLayout />}>
-              <Route element={<HomePage />} index />
-              <Route path="menu" element={<MenuPage />} />
-              <Route path="menu/:productName" element={<MenuDetailPage />} />
-              <Route path="login" element={<LoginPage />} />
-              <Route
-                path="orders"
-                element={
-                  <RequireAuth>
-                    <OrderPage />
-                  </RequireAuth>
-                }
-              />
-              <Route path="*" element={<NotFoundPage />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-        {/*</RouterProvider>*/}
-      </CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route element={<HomePage />} index />
+            <Route path="menu" element={<MenuPage />} />
+            <Route path="menu/:productName" element={<MenuDetailPage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route
+              path="orders"
+              element={
+                <RequireAuth>
+                  <OrderPage />
+                </RequireAuth>
+              }
+            />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ErrorBoundary>
   );
 }
